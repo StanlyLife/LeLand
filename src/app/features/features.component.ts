90e5ff27388959e5adf230830e49884c0f0f1b4d
Stian have <stianhave@hotmail.com>
2020-06-30 20:29:28 +0200

    Added feature component

---
+++ src/app/features/features.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'features',
+  styleUrls: ['./css/features.component.css'],
+  templateUrl: './features.component.html',
+})
+export class FeatureComponent {}
