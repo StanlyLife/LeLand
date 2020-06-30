fdd146a3386f905dfec355b71f6aee11a3a375bd
Stian have <stianhave@hotmail.com>
2020-06-30 18:14:01 +0200

    Added trusted component

---
+++ src/app/trusted/trusted.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trusted-list',
+  styleUrls: ['./css/trusted.component.css'],
+  templateUrl: './trusted.component.html',
+})
+export class TrustedComponent {}
