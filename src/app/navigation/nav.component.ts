811e14fd56a9fd7de46fe1799a696d2420a58d81
Stian have <stianhave@hotmail.com>
2020-06-30 18:13:06 +0200

    Created navbar

---
+++ src/app/navigation/nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./css/nav.component.css'],
+})
+export class NavComponent {}
