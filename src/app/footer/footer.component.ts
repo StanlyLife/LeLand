d672ac2da9b2d20025926fdcd84f9a2a7bf70894
Stian have <stianhave@hotmail.com>
2020-07-01 22:45:07 +0200

    Added footer

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'application-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./css/footer.component.css'],
+})
+export class FooterComponent {}
